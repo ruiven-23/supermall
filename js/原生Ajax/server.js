49dd5efe982a5ffa9c59251afef3b9deeeb2f662
rui <rui@atguigu.com>
2022-07-25 17:28:38 +0800

    text commit

---
+++ js/原生Ajax/server.js
@@ -0,0 +1,31 @@
+// 1. 引入express
+const { request } = require('express');
+const { response } = require('express');
+const express =require('express');
+const { json } = require('express/lib/response');
+
+// 2.创建应用对象
+const app =express();
+
+// 3.创建路由规则   request是对请求报文的封装  response是对响应报文的封装
+app.get('/server',(request, response)=>{
+    // 设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //   设置响应体
+    response.send('hellp server--');
+})
+
+// 4.监听端口启动服务
+app.listen(8000,()=>{
+    console.log("服务已经启动,8000端口监听中....");
+    
+})
+
+//fetch服务
+app.all('/fetch-server',(request,response)=>{
+    // 设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*')
+    //   设置响应体
+    const data ={name: '硅谷'};
+    response.send(JSON.stringify(data));
+})
